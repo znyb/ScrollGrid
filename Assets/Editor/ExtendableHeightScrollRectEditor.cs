0f6eaabf8dacbd9bc3bd2f59041bbdd7d295d656
znyb <znybn1@gmail.com>
2021-02-02 13:48:21 +0800

    Editor

---
+++ Assets/Editor/ExtendableHeightScrollRectEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(ExtendableHeightScrollRect))]
+public class ExtendableHeightScrollRectEditor : ScrollRectEditor
+{
+    SerializedProperty myExtendHeight;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        myExtendHeight = serializedObject.FindProperty("myExtendHeight");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.PropertyField(myExtendHeight);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Editor/ScrollGridEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(ScrollGrid))]
+public class ScrollGridEditor : GridLayoutGroupEditor 
+{
+    SerializedProperty myViewPadding;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        myViewPadding = serializedObject.FindProperty("myVisiblePadding");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.PropertyField(myViewPadding, true);
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
